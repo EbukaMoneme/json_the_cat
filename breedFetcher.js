4cd5885b7d8c1971b75333a2323a3e7e43421986
EbukaMoneme <camoneme@gmail.com>
2021-09-23 22:57:51 +0000

    Modularized testing to use mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,18 @@
 const request = require('request');
-let args = process.argv.slice(2);
-let path = args[0];
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Error: Breed could not be found', null);
+      return;
+    }
+    callback(null, data[0].description);
+  
+  });
+};
 
-
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${path}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Error: Breed could not be found');
-    return;
-  }
-  console.log(data[0].description);
-  console.log(typeof data);
-
-});
+module.exports = { fetchBreedDescription };
